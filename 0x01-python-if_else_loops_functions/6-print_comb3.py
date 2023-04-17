75bcdd26c44ad68bb5c06a56d754bdd620cff313
MrBarns <barnabasoluwatosin@gmail.com>
2023-04-17 08:45:01 +0100

    Add task6 script

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num1 in range(0, 10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == num2 - 1 and num2 == 9:
+            end = "\n"
+        else:
+            end = ", "
+
+        print("{:d}{:d}".format(num1, num2), end=end)
