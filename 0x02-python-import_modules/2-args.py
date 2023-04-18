ddae787948798efd75d9e6bd7393d879b44f3c7f
MrBarns <barnabasoluwatosin@gmail.com>
2023-04-18 15:44:05 +0100

    modify 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-num_args = len(argv)
-ind = 1
-if num_args == 2:
-    print("1 argument:")
+    num_args = len(argv)
+    ind = 1
+    if num_args == 2:
+        print("1 argument:")
 
-elif num_args == 1:
-    print("0 arguments.")
+    elif num_args == 1:
+        print("0 arguments.")
 
-else:
-    print("{:d} arguments:".format(num_args - 1))
+    else:
+        print("{:d} arguments:".format(num_args - 1))
 
-while ind < num_args:
-    print("{:d}: {}".format(ind, argv[ind]))
-    ind += 1
+        while ind < num_args:
+            print("{:d}: {}".format(ind, argv[ind]))
+            ind += 1
