7bb6635c87aeeb01cd3c2e7ca98d424942b31b00
MrBarns <barnabasoluwatosin@gmail.com>
2023-05-22 23:11:47 +0100

    modify task6 exception handling

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,14 +44,13 @@ class Square:
     def position(self, value):
         """sets the value of position attribute"""
 
-        try:
-            if type(value) != tuple or len(value) != 2:
-                raise TypeError
-            if type(value[0]) != int or type(value[1]) != int:
-                raise TypeError
-            if value[0] < 0 or value[1] < 0:
-                raise TypeError
-        except TypeError:
+        if type(value) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
