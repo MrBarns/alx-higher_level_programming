77242ad7ddff378f895a7ee7b49926df8ca8dfe4
MrBarns <barnabasoluwatosin@gmail.com>
2023-05-23 03:15:06 +0100

    Add task102 script

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Create Square class with controls for private and public attribute"""
+
+
+class Square:
+    """Defines class representing a square"""
+
+    def __init__(self, size=0):
+        """Creates instance of type Square
+
+        Args:
+            size (int): length of Square object's sides
+
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """int: size of Square object's sides"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """int: size of Square object's sides"""
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the area of a Square object"""
+
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """Method to compare Square objects"""
+
+        return self.__size == other.__size
+
+    def __lt__(self, other):
+        """Method to compare Square object size"""
+
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        """Method to compare Square object sizes"""
+
+        return self.__size <= other.__size
