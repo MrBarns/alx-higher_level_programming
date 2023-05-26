2cea304b43e47f315c0cc9e9f14fa92771448d94
MrBarns <barnabasoluwatosin@gmail.com>
2023-05-26 18:14:44 +0100

    Add task3 script

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+
+Module defines a function to print a square
+
+"""
+
+
+def print_square(size):
+    """Function to print a square of specified size
+
+    Args:
+        size (int): Size of the square's sides. Must be positive integer
+
+    Returns:
+        Nothing
+
+    Raises:
+        ValueError: if size is less than 0
+        TypeError: if size is not an integer
+
+    """
+
+    try:
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    except Exception as e:
+        raise e
+    else:
+        y = size
+        if y == 0:
+            print()
+        else:
+            while y > 0:
+                x = size
+                while x > 0:
+                    print("#", end="")
+                    x -= 1
+                print()
+                y -= 1
