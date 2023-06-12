f7bac38dc9b8e43813c18ad97ef769c8e3bebda3
MrBarns <barnabasoluwatosin@gmail.com>
2023-06-13 00:42:10 +0100

    Add task6 script

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Create table
+-- Statement to create table with primary key
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+);
