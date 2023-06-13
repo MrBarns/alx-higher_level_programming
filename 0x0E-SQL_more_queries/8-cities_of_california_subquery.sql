fd5cf0f76711f475f85c3c4c65e2ea655efc9016
MrBarns <barnabasoluwatosin@gmail.com>
2023-06-13 02:40:40 +0100

    task8 modify script

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Display cities in California
 -- Select cities of Carlifornia in database
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (
   SELECT id FROM states
   WHERE `name` = "California"
